64f61bbf38dd47d1c021e9ad26e2206e3be1e150
Azad <168946109+freedomzzl@users.noreply.github.com>
2025-10-17 15:08:48 +0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include"recursive_pathoram"
+
+
+void testRecursiveringoram()
+{
+    ServerStorage storage;
+    recursive_pathoram oram(2000, &storage);
+
+
+    for (int i = 0; i < 200; i++)
+    {
+        string test_data = "REAL_RINGORAM_TEST";
+        test_data.resize(blocksize, 'R');
+        vector<char> write_data(test_data.begin(), test_data.end());
+        oram.access(i, ringoram::WRITE, write_data);
+    }
+
+    for (int i = 0; i < 200; i++)
+    {
+        vector<char> read_data = oram.access(i, ringoram::READ, {});
+        cout << "read!" << endl;
+        string result(read_data.begin(), read_data.end());
+        std::cout << result << std::endl;
+    }
+}
+
+
+int main()
+{
+       try {
+    
+    testWithQueryFile("small_query.txt", true);
+}
+      catch (const std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return 1;
+}
+
+return 0;
+
+
+
+}
